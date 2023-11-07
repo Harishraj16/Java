3fd829ae5f49e4b815b62374a15d74864eb17818
Harishraj16 <harishselvaraj27@gmail.com>
2023-11-07 06:38:57 +0530

    added Conditionals_loops file

---
+++ 03. conditionals_loops.java
@@ -0,0 +1,147 @@
+package Harish;
+
+import java.util.Scanner;
+public class conditionals_loops {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        //    Area Of Circle
+//        System.out.print("Enter radius: ");
+//        int r = in.nextInt();
+//        System.out.println("Area of the circle: " + (Math.PI * (r*r)));
+
+        // factors of a number
+//        System.out.print("Enter the number: ");
+//        int n = in.nextInt();
+//        for(int i=1;i<=n;i++){
+//            if(n%i == 0) {
+//                System.out.print(i + " ");
+//            }
+//        }
+
+        // sum until user enter 0
+//        int sum=0;
+//        int n;
+//        while(true){
+//            System.out.print("enter a number: ");
+//            n = in.nextInt();
+//            sum += n;
+//            if(n==0){
+//                System.out.println("Sum: "+sum);
+//                break;
+//            }
+//        }
+      // max until user enter 0
+//        int max=0;
+//        int n;
+//        while(true){
+//            System.out.print("enter a number: ");
+//            n = in.nextInt();
+//            max = Math.max(max,n);
+//            if(n==0){
+//                System.out.println("Max: "+max);
+//                break;
+//            }
+//        }
+
+        // factorial of n
+//        System.out.println("Enter the number: ");
+//        int n = in.nextInt();
+//        int fact=1;
+//        for(int i=1;i<=n;i++){
+//            fact *= i;
+//        }
+//        System.out.println("factorial: "+fact);
+
+        //Average of N numbers
+//        System.out.println("Enter the number: ");
+//        int n=in.nextInt();
+//        int arr[]=new int[n];
+//        for(int i=0;i<n;i++){
+//            arr[i]=in.nextInt();
+//        }
+//        int sum=0;
+//        for(int i=0;i<n;i++){
+//            sum+=arr[i];
+//        }
+//        int average=sum/n;
+//        System.out.println("Average: " + average);
+
+//        int negsum,oddsum,evensum;
+//        negsum=oddsum=evensum=0;
+//        while(true){
+//            System.out.print("Enter the number: ");
+//            int x=in.nextInt();
+//            if(x<0) negsum += x;
+//            else if (x>0){
+//                if(x%2==0) evensum += x;
+//                else oddsum += x;
+//            }
+//            else break;
+//        }
+//        System.out.println("Negative sum: "+negsum);
+//        System.out.println("Even Positive sum: "+evensum);
+//        System.out.println("Odd Positive sum: "+oddsum);
+
+
+    }
+}
+
+
+
+//    Area of Triangle:
+//        Formula: A = 0.5 * base * height
+//
+//        Area of Rectangle:
+//        Formula: A = length * width
+//
+//        Area of Isosceles Triangle:
+//        Formula: A = 0.5 * base * height
+//
+//        Area of Parallelogram:
+//        Formula: A = base * height
+//
+//        Area of Rhombus:
+//        Formula: A = (diagonal1 * diagonal2) / 2
+//
+//        Area of Equilateral Triangle:
+//        Formula: A = (side^2 * √3) / 4
+//
+//        Perimeter of Circle:
+//        Formula: P = 2 * π * r
+//
+//        Perimeter of Equilateral Triangle:
+//        Formula: P = 3 * side
+//
+//        Perimeter of Parallelogram:
+//        Formula: P = 2 * (base + side)
+//
+//        Perimeter of Rectangle:
+//        Formula: P = 2 * (length + width)
+//
+//        Perimeter of Square:
+//        Formula: P = 4 * side
+//
+//        Perimeter of Rhombus:
+//        Formula: P = 4 * side
+//
+//        Volume of Cone:
+//        Formula: V = (1/3) * π * r^2 * h
+//
+//        Volume of Prism:
+//        Formula: V = base area * height
+//
+//        Volume of Cylinder:
+//        Formula: V = π * r^2 * h
+//
+//        Volume of Sphere:
+//        Formula: V = (4/3) * π * r^3
+//
+//        Volume of Pyramid:
+//        Formula: V = (1/3) * base area * height
+//
+//        Curved Surface Area of Cylinder:
+//        Formula: A = 2 * π * r * h
+//
+//        Total Surface Area of Cube:
+//        Formula: A = 6 * side^2
